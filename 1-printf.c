b6e798b9a477d6fb577cde8d3723fe7b0a996565
EDEN <issamghled@gmail.com>
2023-10-15 02:21:43 +0100

    4

---
+++ 1-printf.c
@@ -0,0 +1,55 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * _printf - produces output according to a format
+ * @format: format string
+ * Return: the number of characters printed
+ */
+int _printf(const char *format, ...)
+{
+    va_list args;
+    int count = 0;
+
+    va_start(args, format);
+
+    while (*format)
+    {
+        if (*format != '%')
+        {
+            write(1, format, 1);
+            count++;
+        }
+        else
+        {
+            format++;
+            if (*format == 'c')
+            {
+                char c = va_arg(args, int);
+                write(1, &c, 1);
+                count++;
+            }
+            else if (*format == 's')
+            {
+                char *str = va_arg(args, char *);
+                if (str == NULL)
+                    str = "(null)";
+                while (*str)
+                {
+                    write(1, str, 1);
+                    str++;
+                    count++;
+                }
+            }
+            else if (*format == '%')
+            {
+                write(1, "%", 1);
+                count++;
+            }
+        }
+        format++;
+    }
+
+    va_end(args);
+    return count;
+}
