56f309620968e81de353e69c5fb295d92ee524d5
EDEN <issamghled@gmail.com>
2023-10-17 03:23:50 +0100

    4

--- 1-printf.c
+++ 1-printf.c
@@ -1,68 +1,50 @@
-#include <stdio.h>
 #include <stdarg.h>
+#include <unistd.h>
+#include "main.h"  // Your header file should contain function prototypes
 
-/**
- * _printf - Custom printf implementation.
- * @format: Format string.
- * Return: Number of characters printed (excluding the null byte).
- */
 int _printf(const char *format, ...)
 {
-	va_list args;
-	int printed_chars = 0;
+    va_list args;
+    int count = 0;
 
-	va_start(args, format);
+    va_start(args, format);
 
-	while (*format)
-	{
-		if (*format == '%')
-		{
-			format++;
-			if (*format == 'c')
-			{
-				char c = va_arg(args, int);
-				putchar(c);
-				printed_chars++;
-			}
-			else if (*format == 's')
-			{
-				char *str = va_arg(args, char *);
-				while (*str)
-				{
-					putchar(*str);
-					str++;
-					printed_chars++;
-				}
-			}
-			else if (*format == '%')
-			{
-				putchar('%');
-				printed_chars++;
-			}
-			else
-			{
-				putchar('%');
-				printed_chars++;
-				putchar(*format);
-				printed_chars++;
-			}
-		}
-		else
-		{
-			putchar(*format);
-			printed_chars++;
-		}
-		format++;
-	}
+    while (*format)
+    {
+        if (*format != '%')  // Regular character
+        {
+            write(1, format, 1);  // Print the character
+            count++;
+        }
+        else  // Format specifier
+        {
+            format++;  // Move to the character after '%'
 
-	va_end(args);
+            if (*format == '\0')  // Check for incomplete format specifier
+                break;
 
-	return printed_chars;
-}
+            if (*format == 'c')  // Character specifier
+            {
+                char c = va_arg(args, int);  // Fetch the character argument
+                write(1, &c, 1);  // Print the character
+                count++;
+            }
+            else if (*format == 's')  // String specifier
+            {
+                char *str = va_arg(args, char *);  // Fetch the string argument
+                write(1, str, strlen(str));  // Print the string
+                count += strlen(str);
+            }
+            else if (*format == '%')  // Literal '%' character
+            {
+                write(1, "%", 1);  // Print '%'
+                count++;
+            }
+        }
 
-int main(void)
-{
-	int count = _printf("Hello, %s! This is a %c test. This is a percent sign: %%\n", "world", 'C');
-	printf("\nCharacters printed: %d\n", count);
-	return (0);
+        format++;
+    }
+
+    va_end(args);
+    return count;
 }
