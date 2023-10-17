25d357c0b2a296b6d4f72c64d02ab3f3b21023f5
EDEN <issamghled@gmail.com>
2023-10-17 03:26:15 +0100

    4

--- 1-printf.c
+++ 1-printf.c
@@ -1,72 +1,55 @@
-#include <stdarg.h>
-#include <unistd.h>
 #include "main.h"
+#include <unistd.h>
 
 /**
- * _printf - Print formatted output to stdout
- * @format: The format string containing directives
- *
- * Return: The number of characters printed (excluding the null byte)
+ * _printf - produces output according to a format
+ * @format: format string
+ * Return: the number of characters printed
  */
 int _printf(const char *format, ...)
 {
-	va_list args;
-	int count = 0;
+    va_list args;
+    int count = 0;
 
-	va_start(args, format);
+    va_start(args, format);
 
-	while (*format)
-	{
-		if (*format != '%')  /* Regular character */
-		{
-			write(1, format, 1);  /* Print the character */
-			count++;
-		}
-		else  /* Format specifier */
-		{
-			format++;  /* Move to the character after '%' */
+    while (*format)
+    {
+        if (*format != '%')
+        {
+            write(1, format, 1);
+            count++;
+        }
+        else
+        {
+            format++;
+            if (*format == 'c')
+            {
+                char c = va_arg(args, int);
+                write(1, &c, 1);
+                count++;
+            }
+            else if (*format == 's')
+            {
+                char *str = va_arg(args, char *);
+                if (str == NULL)
+                    str = "(null)";
+                while (*str)
+                {
+                    write(1, str, 1);
+                    str++;
+                    count++;
+                }
+            }
+            else if (*format == '%')
+            {
+                write(1, "%", 1);
+                count++;
+            }
+        }
+        format++;
+    }
 
-			if (*format == '\0')  /* Check for incomplete format specifier */
-				break;
-
-			if (*format == 'c')  /* Character specifier */
-			{
-				char c = va_arg(args, int);  /* Fetch the character argument */
-				write(1, &c, 1);  /* Print the character */
-				count++;
-			}
-			else if (*format == 's')  /* String specifier */
-			{
-				char *str = va_arg(args, char *);  /* Fetch the string argument */
-				write(1, str, _strlen(str));  /* Print the string */
-				count += _strlen(str);
-			}
-			else if (*format == '%')  /* Literal '%' character */
-			{
-				write(1, "%", 1);  /* Print '%' */
-				count++;
-			}
-		}
-
-		format++;
-	}
-
-	va_end(args);
-	return count;
-}
-
-/**
- * _strlen - Calculate the length of a string
- * @s: The string to be measured
- *
- * Return: The length of the string
- */
-int _strlen(char *s)
-{
-	int length = 0;
-
-	while (s[length])
-		length++;
-
-	return (length);
+    va_end(args);
+    return count;
 }
