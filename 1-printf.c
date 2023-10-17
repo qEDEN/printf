179a5ab081b587d3584804a828f2d695287ed99e
EDEN <issamghled@gmail.com>
2023-10-17 03:37:10 +0100

    3

--- 1-printf.c
+++ 1-printf.c
@@ -1,56 +1,89 @@
 #include "main.h"
 #include <unistd.h>
-#include <stdarg.h>  // Include the necessary header for va_list
+#include <stdarg.h>
 
 /**
- * _printf - produces output according to a format
- * @format: format string
- * Return: the number of characters printed
+ * _printf - Produces output according to a format
+ * @format: Format string
+ * Return: The number of characters printed
  */
 int _printf(const char *format, ...)
 {
-    va_list args;
-    int count = 0;
+	va_list args;
+	int count = 0;
 
-    va_start(args, format);
+	va_start(args, format);
+	count = print_formatted_output(format, args);
+	va_end(args);
 
-    while (*format)
-    {
-        if (*format != '%')
-        {
-            write(1, format, 1);
-            count++;
-        }
-        else
-        {
-            format++;
-            if (*format == 'c')
-            {
-                char c = va_arg(args, int);
-                write(1, &c, 1);
-                count++;
-            }
-            else if (*format == 's')
-            {
-                char *str = va_arg(args, char *);
-                if (str == NULL)
-                    str = "(null)";
-                while (*str)
-                {
-                    write(1, str, 1);
-                    str++;
-                    count++;
-                }
-            }
-            else if (*format == '%')
-            {
-                write(1, "%", 1);
-                count++;
-            }
-        }
-        format++;
-    }
+	return (count);
+}
 
-    va_end(args);
-    return count;
+/**
+ * print_formatted_output - Helper function to print formatted output
+ * @format: Format string
+ * @args: Variable arguments
+ * Return: The number of characters printed
+ */
+int print_formatted_output(const char *format, va_list args)
+{
+	int count = 0;
+
+	while (*format)
+	{
+		if (*format != '%')
+		{
+			write(1, format, 1);
+			count++;
+		}
+		else
+		{
+			format++;
+			if (*format == 'c')
+				count += print_char(va_arg(args, int));
+			else if (*format == 's')
+				count += print_string(va_arg(args, char *));
+			else if (*format == '%')
+			{
+				write(1, "%", 1);
+				count++;
+			}
+		}
+		format++;
+	}
+
+	return (count);
+}
+
+/**
+ * print_char - Print a character and return the number of characters printed
+ * @c: Character to be printed
+ * Return: The number of characters printed
+ */
+int print_char(int c)
+{
+	write(1, &c, 1);
+	return (1);
+}
+
+/**
+ * print_string - Print a string and return the number of characters printed
+ * @str: String to be printed
+ * Return: The number of characters printed
+ */
+int print_string(char *str)
+{
+	int count = 0;
+
+	if (str == NULL)
+		str = "(null)";
+
+	while (*str)
+	{
+		write(1, str, 1);
+		str++;
+		count++;
+	}
+
+	return (count);
 }
