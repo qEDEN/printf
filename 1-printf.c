59ecccf197e74996b1436bfa0fbae2b83927e4dd
EDEN <issamghled@gmail.com>
2023-10-17 03:33:54 +0100

    4

--- 1-printf.c
+++ 1-printf.c
@@ -1,50 +1,56 @@
 #include "main.h"
-#include <limits.h>
-#include <stdio.h>
+#include <unistd.h>
+#include <stdarg.h>  // Include the necessary header for va_list
 
 /**
  * _printf - produces output according to a format
- * @format: format string containing the characters and the specifiers
- * Description: this function will call the get_print() function that will
- * determine which printing function to call depending on the conversion
- * specifiers contained into fmt
- * Return: length of the formatted output string
+ * @format: format string
+ * Return: the number of characters printed
  */
 int _printf(const char *format, ...)
 {
-	int (*pfunc)(va_list, flags_t *);
-	const char *p;
-	va_list arguments;
-	flags_t flags = {0, 0, 0};
+    va_list args;
+    int count = 0;
 
-	register int count = 0;
+    va_start(args, format);
 
-	va_start(arguments, format);
-	if (!format || (format[0] == '%' && !format[1]))
-		return (-1);
-	if (format[0] == '%' && format[1] == ' ' && !format[2])
-		return (-1);
-	for (p = format; *p; p++)
-	{
-		if (*p == '%')
-		{
-			p++;
-			if (*p == '%')
-			{
-				count += _putchar('%');
-				continue;
-			}
-			while (get_flag(*p, &flags))
-				p++;
-			pfunc = get_print(*p);
-			count += (pfunc)
-				? pfunc(arguments, &flags)
-				: _printf("%%%c", *p);
-		} else
-			count += _putchar(*p);
-	}
-	_putchar(-1);
-	va_end(arguments);
-	return (count);
+    while (*format)
+    {
+        if (*format != '%')
+        {
+            write(1, format, 1);
+            count++;
+        }
+        else
+        {
+            format++;
+            if (*format == 'c')
+            {
+                char c = va_arg(args, int);
+                write(1, &c, 1);
+                count++;
+            }
+            else if (*format == 's')
+            {
+                char *str = va_arg(args, char *);
+                if (str == NULL)
+                    str = "(null)";
+                while (*str)
+                {
+                    write(1, str, 1);
+                    str++;
+                    count++;
+                }
+            }
+            else if (*format == '%')
+            {
+                write(1, "%", 1);
+                count++;
+            }
+        }
+        format++;
+    }
 
+    va_end(args);
+    return count;
 }
